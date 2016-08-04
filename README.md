89595016ca9d626201b2b0d3e6be85b792b9cd43
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-08-05 02:40:31 +0700

    first commit

--- README.md
+++ README.md
@@ -6,7 +6,23 @@ If you want to understand how the code works, the details are here:
 
 https://realpython.com/blog/python/face-recognition-with-python/
 
-
 Update: Now supports OpenCV3. This change has been made by furetosan ( https://github.com/furetosan) and tested on Linux.
 
-To run the OpenCV3 version, run facedetect_cv3.py.
+To run the OpenCV3 version, run `facedetect_cv3.py`
+
+Replace faces in single image:
+
+```
+python face_replace.py abba.png replacement.png haarcascade_frontalface_default.xml
+```
+
+Convert video to images (frames) + audio and vice versa:
+
+```
+bin/strip video.mp4 video/frames video/audio.aac
+bin/combine video/out video/audio.acc out.mp4
+```
+
+Todo:
+
+* Replace faces in directory
--- face_replace.py
+++ face_replace.py
@@ -3,7 +3,8 @@ import sys
 
 # Get user supplied values
 imagePath = sys.argv[1]
-cascPath = sys.argv[2]
+replacementPath = sys.argv[2]
+cascPath = sys.argv[3]
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
@@ -11,6 +12,7 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 # Read the image
 image = cv2.imread(imagePath)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+imageReplacement = cv2.imread(replacementPath, -1)
 
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
@@ -25,7 +27,13 @@ print "Found {0} faces!".format(len(faces))
 
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    y = y - h / 1.5
+    x = x - w / 1.5
+    h *= 2
+    w *= 2
+    imageReplacementResized = cv2.resize(imageReplacement, (w, h))
+    for c in range(0, 3):
+        image[y:y+h, x:x+w, c] = imageReplacementResized[:, :, c] * (imageReplacementResized[:, :, 3]/255.0) + image[y:y+h, x:x+w, c] * (1.0 - imageReplacementResized[:, :, 3]/255.0)
 
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
